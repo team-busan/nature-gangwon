6c7239d089692a3fb0a1da1bc1fdf8c7cc6e21e7
cheolmin99 <cjfals8570@naver.com>
2024-07-19 14:29:22 +0900

    keyservice삭제, 로그인 기능 완

--- back/src/main/java/com/example/back/controller/AuthController.java
+++ back/src/main/java/com/example/back/controller/AuthController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.back.dto.request.auth.SignInRequestDto;
 import com.example.back.dto.request.auth.SignUpRequestDto;
+import com.example.back.dto.response.auth.SignInResponseDto;
 import com.example.back.dto.response.auth.SignUpResponseDto;
 import com.example.back.service.AuthService;
 
@@ -24,6 +26,14 @@ public class AuthController {
         @RequestBody @Valid SignUpRequestDto requestBody
     ) {
         ResponseEntity<? super SignUpResponseDto> response = authService.signUp(requestBody);
+        return response;
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<? super SignInResponseDto> signIn (
+        @RequestBody @Valid SignInRequestDto requestBody
+    ) {
+        ResponseEntity<? super SignInResponseDto> response = authService.signIn(requestBody);
         return response;
     }
 }
--- back/src/main/java/com/example/back/service/implement/AuthServiceImplement.java
+++ back/src/main/java/com/example/back/service/implement/AuthServiceImplement.java
@@ -6,10 +6,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.back.dto.ResponseDto;
+import com.example.back.dto.request.auth.SignInRequestDto;
 import com.example.back.dto.request.auth.SignUpRequestDto;
+import com.example.back.dto.response.auth.SignInResponseDto;
 import com.example.back.dto.response.auth.SignUpResponseDto;
 import com.example.back.dto.response.user.ValidateNicknameResponseDto;
 import com.example.back.entity.UserEntity;
+import com.example.back.provider.JwtProvider;
 import com.example.back.repository.UserRepository;
 import com.example.back.service.AuthService;
 
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService {
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -46,5 +50,26 @@ public class AuthServiceImplement implements AuthService {
             return ResponseDto.databaseError();
         }
         return SignUpResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
+        String token = null;
+        try {
+            String userEmail = dto.getUserEmail();
+            UserEntity userEntity = userRepository.findByUserEmail(userEmail);
+            if(userEntity == null) SignInResponseDto.singInFail();
+
+            String userPassword = dto.getUserPassword();
+            String encodePassword = userEntity.getUserPassword();
+            boolean isMatched = passwordEncoder.matches(userPassword, encodePassword);
+            if(!isMatched) return SignInResponseDto.singInFail();
+
+            token = jwtProvider.create(userEmail);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return SignInResponseDto.success(token);
     }
 }
