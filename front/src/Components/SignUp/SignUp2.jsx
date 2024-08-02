debd6533e0ede114b6b945fd0e96dae8ddff2a30
Lee jae hyeong <ftko6252@naver.com>
2024-08-02 18:02:03 +0900

    signUp1

--- front/src/Components/SignUp/SignUp2.jsx
+++ front/src/Components/SignUp/SignUp2.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaCircle } from "react-icons/fa";
 
 export default function Signup2({ email }) {
+  const [passwordValid, setPasswordValid] = useState(false);
   const [formData, setFormData] = useState({
     password: "",
     confirmPassword: "",
-    nickname: ""
+    nickname: "",
   });
   const [error, setError] = useState(null);
   const navigate = useNavigate();
@@ -15,8 +18,13 @@ export default function Signup2({ email }) {
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
+
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,19}$/;
+    setPasswordValid(passwordPattern.test(formData.password));
+
+
   };
 
   const validateForm = () => {
@@ -29,14 +37,14 @@ export default function Signup2({ email }) {
   };
 
   const mutation = useMutation({
-    mutationFn: (data) => axios.post('http://localhost:8000/auth/signup', data),
+    mutationFn: (data) => axios.post("http://localhost:8000/auth/signup", data),
     onSuccess: () => {
       alert("회원가입이 완료되었습니다!");
-      navigate('/login');
+      navigate("/login");
     },
     onError: () => {
       setError("회원가입 중 오류가 발생했습니다.");
-    }
+    },
   });
 
   const handleSubmit = (e) => {
@@ -45,17 +53,20 @@ export default function Signup2({ email }) {
     mutation.mutate({
       email,
       password: formData.password,
-      nickname: formData.nickname
+      nickname: formData.nickname,
     });
   };
 
   return (
     <div>
-      <h2 className="text-3xl font-bold text-gray-800 mb-4">회원가입 정보 입력</h2>
-      {error && <p className="text-red-500 mb-4">{error}</p>}
       <form onSubmit={handleSubmit}>
         <div className="form-group mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">Email</label>
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            이메일
+          </label>
           <input
             type="email"
             id="email"
@@ -67,35 +78,68 @@ export default function Signup2({ email }) {
         </div>
 
         <div className="form-group mb-4">
-          <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">비밀번호</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="비밀번호"
-            className="border border-gray-300 p-2 rounded-md w-full"
-            required
-            value={formData.password}
-            onChange={handleChange}
-          />
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            비밀번호
+          </label>
+          <span className="relative">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="비밀번호"
+              className="border border-gray-300 p-2 rounded-md w-full"
+              required
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <span className="absolute top-0 right-5 text-lg">
+              <FaEyeSlash />
+            </span>
+          </span>
+          <div className = "flex items-center mt-1">
+            <FaCircle className={passwordValid ? "text-green mr-1 text-xs" : "text-red-600 mr-1 text-xs"} />
+            <p>{passwordValid ? "8자 이상 20자 미만입니다." : "8자 이상 20자 미만으로 입력해주세요"}</p>
+          </div>
+          <div className = "flex items-center mt-1">
+            <FaCircle className={passwordValid ? "text-green mr-1 text-xs" : "text-red-600 mr-1 text-xs"} />
+            <p>{passwordValid ? "숫자 또는 문자가 하나 이상입니다" : "숫자 또는 문자 하나 이상 입력해주세요"}</p>
+          </div>
         </div>
 
         <div className="form-group mb-4">
-          <label htmlFor="confirmPassword" className="block text-gray-700 font-semibold mb-2">비밀번호 확인</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="비밀번호 확인"
-            className="border border-gray-300 p-2 rounded-md w-full"
-            required
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
+          <label
+            htmlFor="confirmPassword"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            비밀번호 확인
+          </label>
+          <span className="relative">
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="비밀번호 확인"
+              className="border border-gray-300 p-2 rounded-md w-full"
+              required
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+            <span className="absolute top-0 right-5 text-lg">
+              <FaEyeSlash />
+            </span>
+          </span>
         </div>
 
         <div className="form-group mb-6">
-          <label htmlFor="nickname" className="block text-gray-700 font-semibold mb-2">닉네임</label>
+          <label
+            htmlFor="nickname"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            닉네임
+          </label>
           <input
             type="text"
             id="nickname"
@@ -108,7 +152,10 @@ export default function Signup2({ email }) {
           />
         </div>
 
-        <button type="submit" className="w-full bg-gray-400 text-white p-3 rounded-md font-semibold hover:bg-green transition duration-300">
+        <button
+          type="submit"
+          className="w-full bg-gray-400 text-white p-3 rounded-md font-semibold hover:bg-green transition duration-300"
+        >
           회원가입
         </button>
       </form>
