fedccd53591d839608ba3018c892af43d10a1a76
hongchun <jhc5317@gmail.com>
2024-09-26 19:43:17 +0900

    web.js change

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Web from "./Web";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
@@ -18,7 +18,7 @@ root.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <App />
+      <Web />
     </QueryClientProvider>
   </RecoilRoot>
 );
