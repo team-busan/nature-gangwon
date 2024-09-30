f08485de25412a72251323d3f7f90e0143465788
hongchun <jhc5317@gmail.com>
2024-09-30 12:25:44 +0900

    여행 계획 수정 지도 마커 버그 수정, 콘솔 로그 삭제

--- front/src/Components/Plan/PlanChooseThumbnailEdit.jsx
+++ front/src/Components/Plan/PlanChooseThumbnailEdit.jsx
@@ -77,7 +77,6 @@ const PlanChooseThumbnailEdit = ({ setPlanStage, dates }) => {
         navigate("/plan/list");
       })
       .catch((err) => {
-        console.log(err);
         Swal.fire({
           icon: "error",
           title: "계획 저장에 실패했습니다.",
